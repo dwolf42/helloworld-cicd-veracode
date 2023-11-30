e8d93db58e299d13368a5d5e3d2375f675b529e2
David Wolf <david.wolf@ktc.de>
2023-11-30 14:21:12 +0100

    test attempt 001

--- src/main/java/de/ktc/HelloWorld.java
+++ src/main/java/de/ktc/HelloWorld.java
@@ -1,7 +1,10 @@
 package de.ktc;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(sayHello("World"));
+    }
+    public static String sayHello(String wordToSay) {
+        return "Hello, " + wordToSay + "!";
     }
 
 }
---
+++ src/test/java/de/ktc/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package de.ktc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    void mainUnterTest() {
+        Assertions.assertEquals("Hello, World!", HelloWorld.sayHello("World"));
+        Assertions.assertNotEquals("Hello, Jupiter", HelloWorld.sayHello("Pluto"));
+    }
+}
