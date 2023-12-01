ef6d3bfb25c64497a5f13873de0a6c5a4bc778fa
David Wolf <david.wolf@ktc.de>
2023-12-01 16:21:29 +0100

    github workflow

--- src/main/java/de/ktc/HelloWorld.java
+++ src/main/java/de/ktc/HelloWorld.java
@@ -2,6 +2,7 @@ package de.ktc;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(sayHello("World"));
+
     }
 
     public static String sayHello(String wordToSay) {
