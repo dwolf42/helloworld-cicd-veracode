cda1a9c29e545ce5869416145b77f36f44eb1317
David Wolf <david.wolf@ktc.de>
2023-12-01 09:17:53 +0100

    feat: add test reports and Veracode support

--- src/main/java/de/ktc/HelloWorld.java
+++ src/main/java/de/ktc/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(sayHello("World"));
     }
+
     public static String sayHello(String wordToSay) {
         return "Hello, " + wordToSay + "!";
     }
