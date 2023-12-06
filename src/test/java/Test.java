7c829ce236ee0bae7a515c6bcedcc6ea78c5f850
David Wolf <david.wolf@ktc.de>
2023-12-06 11:56:20 +0100

    rename test file so maybe tests will be executed then

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,10 +1,9 @@
 import de.ktc.HelloWorld;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-class HelloWorldTest {
+class Test {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void mainUnterTest() {
         Assertions.assertEquals("Hello, ----World!", HelloWorld.sayHello("World"));
         Assertions.assertNotEquals("Hello, Jupiter", HelloWorld.sayHello("Pluto"));
